004b18a076f06755bb0b867d376e3129d0eb2a93
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2024-10-15 23:22:50 +0300

    Templates added. Projects dependencies updated.

--- FitOnBlog/EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ FitOnBlog/EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a930086312ae41e4f6cfa5708e79621d6cac8a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa2c10e41a42d25c4ace491c10fb69874641bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
