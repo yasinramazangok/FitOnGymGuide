1f09a484472f6d07d06244b8e930661f1038ba6b
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2024-11-26 19:04:30 +0300

    This is a test commit.

--- FitOnBlog/EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ FitOnBlog/EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e75ffa5b26d68fee0967fc64c6f507e2939c2b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200ba07be8351db284ab09c1e8e942b8fb05b2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
